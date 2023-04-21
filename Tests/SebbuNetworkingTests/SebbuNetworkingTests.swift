3d62e9d0fe2e44f7efa17db1848aa5f4b0e349ae
Martin <sebastian.toivonen@gmail.com>
2023-04-21 20:35:15 +0300

    Update SebbuNetworkingTests.swift

--- Tests/SebbuNetworkingTests/SebbuNetworkingTests.swift
+++ Tests/SebbuNetworkingTests/SebbuNetworkingTests.swift
@@ -5,26 +5,26 @@ final class SebbuNetworkingTests: XCTestCase {
     func testNetworkUtils() {
         let ipAddress = NetworkUtils.publicIP
         XCTAssert(ipAddress != nil, "IP Address was nil")
-        XCTAssert(ipAddress!.isIpAddress())
+        XCTAssert(ipAddress!.isIpAddress)
     }
     
     func testIpAddressRecognition() {
-        XCTAssertTrue("1.2.3.4".isIpAddress())
-        XCTAssertTrue("0.0.0.0".isIpAddress())
-        XCTAssertTrue("255.255.255.255".isIpAddress())
-        XCTAssertTrue("::".isIpAddress())
+        XCTAssertTrue("1.2.3.4".isIpAddress)
+        XCTAssertTrue("0.0.0.0".isIpAddress)
+        XCTAssertTrue("255.255.255.255".isIpAddress)
+        XCTAssertTrue("::".isIpAddress)
         XCTAssertTrue("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff".isIpAddress())
-        XCTAssertTrue("2001::f:1234".isIpAddress())
+        XCTAssertTrue("2001::f:1234".isIpAddress)
         
-        XCTAssertFalse("".isIpAddress())
-        XCTAssertFalse("foo".isIpAddress())
-        XCTAssertFalse("256.256.256.256".isIpAddress())
-        XCTAssertFalse("1.2.3".isIpAddress())
-        XCTAssertFalse("1.2.3.4.5".isIpAddress())
-        XCTAssertFalse("-1.2.3.4".isIpAddress())
-        XCTAssertFalse(":".isIpAddress())
-        XCTAssertFalse("2001::f::1234".isIpAddress())
-        XCTAssertFalse("2001:g::".isIpAddress())
+        XCTAssertFalse("".isIpAddress)
+        XCTAssertFalse("foo".isIpAddress)
+        XCTAssertFalse("256.256.256.256".isIpAddress)
+        XCTAssertFalse("1.2.3".isIpAddress)
+        XCTAssertFalse("1.2.3.4.5".isIpAddress)
+        XCTAssertFalse("-1.2.3.4".isIpAddress)
+        XCTAssertFalse(":".isIpAddress)
+        XCTAssertFalse("2001::f::1234".isIpAddress)
+        XCTAssertFalse("2001:g::".isIpAddress)
     }
     
     #if !os(Windows)
