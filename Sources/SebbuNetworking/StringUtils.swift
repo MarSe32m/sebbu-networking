fd90ac318c8f4c4494d7408c177f3803cc46cc51
Martin <sebastian.toivonen@gmail.com>
2023-04-21 20:33:34 +0300

    Update StringUtils.swift

--- Sources/SebbuNetworking/StringUtils.swift
+++ Sources/SebbuNetworking/StringUtils.swift
@@ -14,7 +14,15 @@ import WinSDK
 #endif
 
 public extension String {
-    func isIpAddress() -> Bool {
+    var isIpAddress: Bool {
+        #if os(Windows)
+        var v4: IN_ADDR = IN_ADDR()
+        var v6: IN6_ADDR = IN6_ADDR()
+        return self.withCString(encodedAs: UTF16.self) {
+            return InetPtonW(AF_INET, $0, &v4) == 1 ||
+                   InetPtonW(AF_INET6, $0, &v6) == 1
+        }
+        #else
         var ipv4Addr = in_addr()
         var ipv6Addr = in6_addr()
 
@@ -22,5 +30,6 @@ public extension String {
             return inet_pton(AF_INET, ptr, &ipv4Addr) == 1 ||
                    inet_pton(AF_INET6, ptr, &ipv6Addr) == 1
         }
+        #endif   
     }
 }
