aea96bf2e357c61c4cc9d2ee96e549f2bd353abf
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2024-08-05 21:54:43 +0300

    Edit Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "sebbu-networking",
     platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SebbuNetworking",
             targets: ["SebbuNetworking"]),
@@ -18,8 +17,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SebbuNetworking",
             dependencies: ["CSebbuNetworking",
